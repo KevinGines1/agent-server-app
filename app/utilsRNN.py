c09d61c67815525ce4390e72fea9693d2fc76e1c
KevinGines1 <kagines1@up.edu.ph>
2022-05-21 18:09:35 +0800

    update model imports (3)

--- app/utilsRNN.py
+++ app/utilsRNN.py
@@ -9,7 +9,7 @@ np.set_printoptions(threshold=np.inf)
 MAX_JOINT_ANGLES_LEN = 325
 NUM_JOINT_ANGLE_PAIRS = 12
 classes = ['CORRECT', 'WRONG']
-model = tf.keras.models.load_model('./app/gruModel') # model built with GRU & Masking
+model = tf.keras.models.load_model('gruModel') # model built with GRU & Masking
 
 
 def logError(error, errorsList): # * fxn that logs the errors to a text file, for debugging purposes
